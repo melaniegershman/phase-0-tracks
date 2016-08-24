d623e93f002d8ed6121416dda858f18d3dbe5387
Melanie Gershman <melanie.gershman@gmail.com>
2016-08-24 00:37:51 -0400

    Write conditional statements (rls 2) and write loop to process multiple employees (rls 3)

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,29 @@
-puts "What is your name?"
-name = gets.chomp
-puts "How old are you? What year were you born?"
-age = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
+def vampire_interview(employees)
+	employees == i 
+	until employees == 0 do
+		puts "What is your name?"
+		name = gets.chomp
+		puts "How old are you? What year were you born?"
+		year = gets.chomp
+		age = gets.chomp
+		puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+		garlic = gets.chomp
+		puts "Would you like to enroll in the company’s health insurance?"
+		insurance = gets.chomp
+
+		if (2016 - year == age) && (garlic == "yes" || insurance == "yes")
+			p "Probably not a vampire."
+		elsif (2016 - year !== age) && (garlic == "no" || insurance == "no")
+			p "Probable a vampie."
+		elsif (2016 - year !== age) && (garlic == "no" && insurance == "no")
+			p "Almost certainly a vampire."
+		elsif name == "Drake Cula" || name == "Tu Fang"
+			p "Definitely a vampire."
+		else
+			p "Results inconclusive."
+		end
+		i -= 1
+	end
+end
