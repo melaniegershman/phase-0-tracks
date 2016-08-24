9eac0c4eabcfa5f6076b8d78b596bd5e61457970
Melanie Gershman <melanie.gershman@gmail.com>
2016-08-24 00:05:57 -0400

    Add release one questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you? What year were you born?"
+age = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
