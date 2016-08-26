99c0fda09359d5e75d97702edd121b5d9b0fd014
Melanie Gershman <melanie.gershman@gmail.com>
2016-08-25 23:54:43 -0400

    Add space exclusion in encryptor, add decryptor code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -10,17 +10,35 @@
 # - Take index of letter within alphabet
 # - Then subtract one from that index value
 # - Use the answer to find the letter
-	# alphabet="abcdefghijklmnopqrstuvwxyz"
+# alphabet="abcdefghijklmnopqrstuvwxyz"
 # letter = alphabet.index("d")
 # letter -= 1
 
 #Encryptor
 def encryptor(msg)
 	i = 0
+	special_char = " !@#$%^&*()?/<>~"
 	while i < msg.length
-	  msg[i] = msg[i].next!
-	  i += 1
+		while special_char.index(msg[i])
+			i += 1
+		end		
+		msg[i] = msg[i].next
+		i += 1
 	end
 	puts msg
 end
 
+#Decryptor 
+def decryptor(msg)
+	i = 0
+	decrypted = "" #assign empty string to collect letters
+	while i < msg.length
+		alphabet = "abcdefghijklmnopqrstuvwxyz" 
+		letter = msg[i]
+		pre_alpha_index = alphabet.index(letter)
+		pre_alpha_index -= 1
+	  	decrypted += alphabet[pre_alpha_index] 
+	i += 1
+	end
+	puts decrypted
+end
