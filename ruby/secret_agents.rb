ac9260fecab8632700b5e2fc1d75aa837e3f7ced
Melanie Gershman <melanie.gershman@gmail.com>
2016-08-26 18:02:20 -0400

    Add interface, release 5

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,3 +1,4 @@
+# --> Paired with Travis Buckley. <--
 # Encrypt method:
 # Takes every letter within a string and moves it forward by one. 
 # - Accept a string
@@ -20,6 +21,14 @@
 # letter -= 1
 # - Test for edgecase, z should be followed by a, not ab.
 
+# Interface pseydocode:
+# Ask user for password, and store password.
+# Ask the user IF they want to encrypt or decrypt. 
+# Intake user input.
+# Runs user preference & prints results.
+# Program ends with self-destruct message.
+
+
 #Encryptor
 def encryptor(msg)
 	i = 0
@@ -37,18 +46,18 @@ def encryptor(msg)
 end
 
 #DRIVER CODE:
-p encryptor("abc")
-p encryptor("zed")
+# p encryptor("abc")
+# p encryptor("zed")
 
 #Decryptor 
 def decryptor(msg)
 	i = 0
 	alphabet = "abcdefghijklmnopqrstuvwxyz" 
 	while i < msg.length
-		# if msg[i] == "z"
-		# 	msg[i] = "a"
-		# elsif msg[i] == " "
-		# 	msg[i] = msg[i]
+		if msg[i] == "z"
+			msg[i] = "a"
+		elsif msg[i] == " "
+			msg[i] = msg[i]
 		else
 			alpha_index = (alphabet.index(msg[i])) - 1 
 		  	msg[i] = alphabet[alpha_index]
@@ -59,11 +68,35 @@ def decryptor(msg)
 end
 
 #DRIVER CODE:
-p decryptor("bcd")
-p decryptor("afe")
+# p decryptor("bcd")
+# p decryptor("afe")
 
-p decryptor(encryptor("swordfish"))
+# p decryptor(encryptor("swordfish"))
 
 #The order of operations is what allows nesting encryptor inside of decryptor to work. 
 # Encryptor encrypts "swordfish", which gives us a new parameter ("txpsegjti") to be decoded 
 # by decryptor. Decryptor accepts the new string and proceeds to decode "txpsegjti".
+
+#OUR DRIVER CODE:
+# ask for password and store
+puts "What is your secret password?"
+password = gets.chomp
+
+# ask whether to encrypt or decrypt
+puts "Encryptor or decryptor?"
+user_input = gets.chomp.downcase
+if user_input == "encryptor"
+	p encryptor(password)
+elsif user_input == "decryptor"
+	p decryptor(password)
+end
+
+#Exit program.
+puts "This message will self-destruct in 5 seconds."
+i = 5
+while i > 0
+	puts "#{i}..."
+	i = i - 1
+	sleep 1
+end
+puts "BOOM!"
