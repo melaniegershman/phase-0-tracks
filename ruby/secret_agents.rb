cc40120c44cf1b109c9e414303bca86c8c1a068b
Melanie Gershman <melanie.gershman@gmail.com>
2016-08-26 16:14:30 -0400

    Add space clause to decryptor code; add edge case

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,42 +3,56 @@
 # - Accept a string
 # - Go through each letter in the string
 # - Advance each character within string
-# - Store new string.
-# - Return new string.
+# - Store new string
+# - Return new string
+# - Test for edgecase, z should be followed by a, not ab.
+
+
 # Decrypt method:
+# Reverses the encryptor.
 # - Accept a string
 # - Take index of letter within alphabet
 # - Then subtract one from that index value
 # - Use the answer to find the letter
+# - Store new string
 # alphabet="abcdefghijklmnopqrstuvwxyz"
 # letter = alphabet.index("d")
 # letter -= 1
+# - Test for edgecase, z should be followed by a, not ab.
 
 #Encryptor
 def encryptor(msg)
 	i = 0
-	special_char = " !@#$%^&*()?/<>~"
 	while i < msg.length
-		while special_char.index(msg[i])
-			i += 1
-		end		
-		msg[i] = msg[i].next
+		if msg[i] == "z"
+			msg[i] == "a"
+		elsif msg[i] == " "
+			msg[i] = msg[i]
+		else
+			msg[i] = msg[i].next
+		end
 		i += 1
 	end
-	puts msg
+	return msg
 end
 
 #Decryptor 
 def decryptor(msg)
 	i = 0
-	decrypted = "" #assign empty string to collect letters
+	alphabet = "abcdefghijklmnopqrstuvwxyz" 
+	#assign empty string to collect letters
+	decrypted = "" 
 	while i < msg.length
-		alphabet = "abcdefghijklmnopqrstuvwxyz" 
-		letter = msg[i]
-		pre_alpha_index = alphabet.index(letter)
-		pre_alpha_index -= 1
-	  	decrypted += alphabet[pre_alpha_index] 
+		if msg[i] == "z"
+			msg[i] = "a"
+		elsif msg[i] == " "
+			decrypted[i] = msg[i]
+		else
+			alpha_index = (alphabet.index(msg[i])) - 1
+		  	decrypted[i] = alphabet[alpha_index]
+		end
 	i += 1
 	end
-	puts decrypted
+	return decrypted
 end
+
