7375315b699e4b6f41787b547bf509924188cb6c
Melanie Gershman <melanie.gershman@gmail.com>
2016-08-30 13:55:27 -0400

    Compare .each and .map for Release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+#RELEASE 0
+
 def greeting
 	name1 = "Melanie"
 	name2 = "Tim"
@@ -7,3 +9,34 @@ def greeting
 end
 
 greeting {|name1, name2| puts "#{name1} and #{name2} paired together on this assignment."}
+
+#RELEASE 1
+colors = ["blue", "yellow", "red"]
+objects = {
+	blue: "bird",
+	yellow: "banana",
+	red: "apple"
+}
+
+# iterate through the array with .each
+puts "Original data:"
+p colors
+puts "After .each call:"
+colors.each {|color| puts color}
+p colors
+
+# iterate through the array with .map!
+puts "Original data:"
+p colors
+puts "After .map! call:"
+colors.map! {|color| p "#{color.capitalize} is a primary color!"}
+p colors
+
+# iterate through the hash with .each
+puts "Original data:"
+p objects
+puts "After .each call:"
+objects.each do |key, value| 
+	p "A #{value} is #{key}."
+end
+p objects
