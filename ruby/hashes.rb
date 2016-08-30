6797b3614fd3a87a4e49ee2a6a85ca482a804510
Melanie Gershman <melanie.gershman@gmail.com>
2016-08-30 00:40:04 -0400

    Add option to update client form and maintain appropriate class type

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,17 +1,6 @@
-# Write a program that will allow an interior designer to enter the details
-# of a given client: the client's name, age, number of children, decor theme
-# and more. 
+# Write a program that will allow an interior designer to enter the details of a given client: 
+# the client's name, age, number of children, decor theme and more. 
 
-# Convert any user input to the appropriate data type.
-
-# Give the user the opportunity to update a key (no need to loop, once is fine). 
-# After all, sometimes users make mistakes! If the designer says "none", skip it. 
-# But if the designer enters "decor_theme" (for example), your program should ask 
-# for a new value and update the :decor_theme key. (Hint: Strings have methods that 
-# will turn them into symbols, which would be quite handy here.) You can assume the 
-# user will correctly input a key that exists in your hash -- no need to handle user 
-# errors.
-# Print the latest version of the hash, and exit the program.
 # Helper methods to use in final program
 def name
 	puts "What is your client's full name?"
@@ -37,6 +26,29 @@ def color
 	puts "What is your client's favorite color?"
 	color = gets.chomp
 end
+# Give the user the opportunity to update a key. If the designer says "none", skip it. 
+# Your program should ask for a new value and update the key.
+def update(hash)
+	puts "Here is a summary of your client's information: #{hash}. Would you like to make changes? Type done if you are finished."
+	input = gets.chomp
+	if input != "done"
+		puts "State the name of the preference you would like to update: name, age, children, color."
+		# Convert any user input to the appropriate data type.
+		updated_key = gets.chomp.to_sym
+		puts "How can I update that?"
+
+		if updated_key == :age || updated_key == :children
+			new_value = gets.chomp.to_i
+		else
+			new_value = gets.chomp
+		end	
+
+		hash[updated_key] = new_value
+	end
+	puts "Thank you for using Interior Solutions."	
+	return hash 
+end
+
 # Prompt the designer/user for all of this information (define a method).
 def client_form
 	client_pref = {
@@ -51,9 +63,9 @@ def client_form
 	client_pref[:children] = children
 	client_pref[:decor] = decor
 	client_pref[:color] = color
-	# Print the hash back out to the screen.
+	client_pref = update(client_pref)
+	# Print the latest version of the hash, and exit the program.
 	puts "Here is a summary of your client's information: #{client_pref}."
 end
-
 
 client_form
