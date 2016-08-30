e05bb54ddfe6e6551b84d74da4db88a3a50a05be
Melanie Gershman <melanie.gershman@gmail.com>
2016-08-29 22:21:28 -0400

    Add age, children, decor, and color methods.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,12 +2,8 @@
 # of a given client: the client's name, age, number of children, decor theme
 # and more. 
 
-# Keys should be symbols.
-
-# Prompt the designer/user for all of this information (define a method).
 # Convert any user input to the appropriate data type.
-# Print the hash back out to the screen when the designer has answered all of 
-# 	the questions.
+
 # Give the user the opportunity to update a key (no need to loop, once is fine). 
 # After all, sometimes users make mistakes! If the designer says "none", skip it. 
 # But if the designer enters "decor_theme" (for example), your program should ask 
@@ -16,22 +12,47 @@
 # user will correctly input a key that exists in your hash -- no need to handle user 
 # errors.
 # Print the latest version of the hash, and exit the program.
-
+# Helper methods to use in final program
 def name
-	puts "What is your full name?"
-	client_name = gets.chomp
+	puts "What is your client's full name?"
+	name = gets.chomp
 end
 
+def age
+	puts "What is your client's age?"
+	age = gets.chomp.to_i
+end
+
+def children
+	puts "How many children does your client have?"
+	children = gets.chomp.to_i
+end
+
+def decor
+	puts "What is your client's preferred decor style?"
+	decor = gets.chomp
+end
+
+def color
+	puts "What is your client's favorite color?"
+	color = gets.chomp
+end
+# Prompt the designer/user for all of this information (define a method).
 def client_form
-client_pref = {
-	name: "",
-	age: '',
-	children: '',
-	decor: "",
-}
+	client_pref = {
+		name: "",
+		age: '',
+		children: '',
+		decor: "",
+	}
 	puts "Hello, welcome to Interior Solutions. Please help us help you by answering the following questions."
 	client_pref[:name] = name
-	p client_pref
+	client_pref[:age] = age
+	client_pref[:children] = children
+	client_pref[:decor] = decor
+	client_pref[:color] = color
+	# Print the hash back out to the screen.
+	puts "Here is a summary of your client's information: #{client_pref}."
 end
 
 
