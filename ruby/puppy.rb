7dd19ff9d99c11917e879abc8d3c2026b57c9784
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-05 17:57:24 -0400

    Add characteristics and behavior for puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+Class: Puppy
+SPECIES ------------------------
+Canis familiaris
+
+CHARACTERISTICS ----------------
+Bark: varies
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+Cute: yes
+
+BEHAVIOR -----------------------
+Jump
+Snuggle
+Run
+Play
+=end
