cd59b85510426b750125644f6dc2564530c896ce
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-06 12:03:03 -0400

    Make 50 instances of class, insert into data structure, iterate over data structure

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,5 @@
 class Puppy
-  def initialie
+  def initialize
   	puts "Initializing new puppy instance ..."
   end
 
@@ -37,3 +37,40 @@ fido.speak(3)
 fido.roll_over
 fido.dog_years(2)
 fido.dig("bone")
+
+class Dance
+	def initialize
+		puts "Initializing new dance instance ..."
+	end
+
+	def pirouette(num)
+		num.times do
+			puts "*spins*"
+		end
+	end
+
+	def leap(height)
+		leap_length = height * 5
+		# puts "The dancer leapt #{leap_length} feet!"
+	end
+
+end
+
+ballerina = Dance.new
+ballerina.leap(3)
+ballerina.pirouette(2)
+
+ballerinas = []
+for i in (1..50) do
+	ballerina = Dance.new
+	ballerinas.push(ballerina)
+	# ballerinas.each.with_index { |val,index| puts "index: #{index} for #{val}"}
+end
+
+ballerinas.each do |dancer|  
+	dancer.pirouette(1)
+	dancer.leap(2)
+	# Separates each dancer's actions:
+	# puts "-" * 10
+	end
+# ballerinas[3]
