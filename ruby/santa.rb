f165caed00414a59b3950bcbe86134e6fd325b49
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-07 14:53:51 -0400

    Refactor getter and setters, release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,9 @@
 class Santa
+	# refactor getter method:
+	attr_reader :age, :ethnicity
+	# refactor setter method:
+	attr_accessor :gender
+
 	def initialize(gender, ethnicity)
 		# puts "Initializing Santa instance..."
 		@gender = gender
@@ -16,12 +21,10 @@ class Santa
 	end
 
 	def attributes
-		puts "Santa is #{@gender} and #{@ethnicity}"
-		puts "Santa is #{@age} years old."
-		puts "Santa prefers his reindeer in the following order:"
-		@reindeer_ranking.each_with_index {|reindeer, index| puts "#{index + 1}: #{reindeer}"}
+		puts "Santa is #{@gender}."
 	end
-	# setter methods:
+
+	# setter methods - longhand:
 	def celebrate_birthday
 		@age += 1
 		puts "Santa is now #{@age} years old."
@@ -33,21 +36,22 @@ class Santa
 		@reindeer_ranking.each_with_index {|reindeer, index| puts "#{index + 1}: #{reindeer}"}
 	end
 
-	def gender=(new_gender)
-		@gender = new_gender
-	end
+	# def gender=(new_gender)
+	# 	@gender = new_gender
+	# end
 
-	# getter methods
-	def age
-		@age
-	end
+	# getter methods - longhand:
+	# def age
+	# 	@age
+	# end
 
-	def ethnicity
-		@ethnicity
-	end
+	# def ethnicity
+	# 	@ethnicity
+	# end
 end
 # Empty array to collect the diverse santas
 santas = []
+
 # Push each individual santa into the santa array
 # santas << Santa.new("agender", "black")
 # santas << Santa.new("female", "Latino")
@@ -56,6 +60,7 @@ santas = []
 # santas << Santa.new("female", "prefer not to say")
 # santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
+
 # Gender and ethnicity arrays
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
@@ -64,24 +69,28 @@ example_ethnicities << "Jewish"
 example_genders << "transgender"
 #Initialize many diverse santas (iterate over the gender and ethnicity arrays)
 example_genders.length.times do |i|
+	puts "Creating a #{example_genders[i]} Santa..."
   santas << Santa.new(example_genders[i], example_ethnicities[i])
+  puts "There are now #{santas.length} Santa instances in this array."
 end
 
 # Tell santas what to do
 santas.each do |santa| 
 	santa.speak
 	santa.eat_milk_and_cookies("Snickerdoodle")
-	santa.attributes
 	santa.celebrate_birthday
-	santa.get_mad_at("Vixen")
 end
+
+# Santa gets mad at a reindeer
+st_nick = Santa.new("female", "Russian")
+st_nick.get_mad_at("Vixen")
+
 # Change santa's gender
-puts "-" * 10
-santa_new = Santa.new("male", "Chinese")
-santa_new.attributes
-santa_new.gender = "female"
-santa_new.attributes
-puts "-" * 10
+elf = Santa.new("male", "Chinese")
+puts "The elf is #{elf.gender}."
+elf.gender = "female"
+puts "The elf is now #{elf.gender}!"
+
 # Get santa's age and ethnicity:
-santa_new2 = Santa.new("male", "Chinese")
+santa_new2 = Santa.new("male", "French")
 puts "Santa is #{santa_new2.age} years old and #{santa_new2.ethnicity}"
