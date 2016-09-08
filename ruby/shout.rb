9b20877e16f2610a10adea21f57d97524a349ee4
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-07 23:49:30 -0400

    Convert a standalone module to a mixin, release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -3,16 +3,41 @@
 # RELEASE 1: Write a Simple Module
 
 # Declare a module:
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!! :("
+# 	end
+# 	# Add a yelling happily method
+# 	def self.yelling_happily(words)
+# 		words + "!!! :D"
+# 	end
+# end
+
+# RELEASE 3: Convert a Standalone Module to a Mixin
 module Shout
-	def self.yell_angrily(words)
-		words + "!!! :("
+	def yelling_angrily(words)
+		words.upcase + "!!! >:o"
 	end
-	# Add a yelling happily method
-	def self.yelling_happily(words)
-		words + "!!! :D"
+
+	def yelling_happily(words)
+		words.upcase + "!!! :D"
 	end
 end
-# Driver code to test Shout module
-puts Shout.yell_angrily("you peddled backwards, now you ruined the ride")
 
-puts Shout.yelling_happily("way to go")
+class Rooster
+	include Shout
+end
+
+class PrDirector
+	include Shout
+end
+
+rooster = Rooster.new
+puts rooster.yelling_happily("Cockadoodledoo")
+
+pr_director = PrDirector.new
+puts pr_director.yelling_angrily("the internet is broken")
+
+# Driver code to test Shout module (from release 1)
+# puts Shout.yell_angrily("you peddled backwards, now you ruined the ride")
+# puts Shout.yelling_happily("way to go")
