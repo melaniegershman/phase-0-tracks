f95f7f0b28efbfd2905efa6262a4ba34d2f9effa
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-09 15:01:55 -0400

    Update pseudocode

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,20 +1,20 @@
 =begin
 - Store items on initialization
 input: n/a, list exists when you create an instance
-steps: 
+steps: return list
 output: returns a list of things to do
 - Add item to list
-input:
-steps:
-output:
+input: an item to do
+steps: add item to the end of your to-do list
+output: return updated todo list
 - Delete item
-input:
-steps:
-output:
+input: an item to delete
+steps: remove in to-do list
+output: return updated to-do list
 - Retrieve item by index
-input:
-steps:
-output:
+input: an integer
+steps: find an item on the list by its index
+output: one item
 =end
 
 class TodoList
