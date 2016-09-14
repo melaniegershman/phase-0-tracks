4ad9a23648514639ebf491a3e7a5b48368c2b588
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-14 14:33:35 -0400

    Add code to push new name and color to arrays

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["black", "blue", "green", "purple"];
+var names = ["Jose", "Maria", "Eduardo", "Rachel"];
+
+
+colors.push("pink")
+names.push("Carlos")
+
+console.log(colors)
+console.log(names)
