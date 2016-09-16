12a2cc46f6d9c8c6f91034b1c43a0c7162af6023
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-16 00:11:08 -0400

    Add algos.js file and fill with pseudocode

---
+++ js/algos.js
@@ -0,0 +1,29 @@
+/* RELEASE 0 - FIND THE LONGEST PHRASE
+Take an array and find the length of each of the strings inside it
+Find the index of the largest number
+Use that index to find the longest phrase in the array
+*/
+
+var phraseArr = ["long phrase","longest phrase","longer phrase"]
+function longestPhrase(arr) {
+	for (var i = 0; i < arr.length; i++) {
+  	return Math.max(...i.length);
+	}
+	// Math.max(...arr)
+}
+
+console.log(longestPhrase(phraseArr))
+
+/* RELEASE 1 - FIND A KEY-VALUE MATCH
+a function that takes two objects and checks to see if the objects share at least one key-value pair. 
+If no pairs match (and keep in mind that the two objects may not even have any of the same keys), the function should return false. 
+To make your life easier, don't worry about whether a property is a string ('age') or an identifier name (age). Those can be considered equivalent. 
+Again, try to reason through the problem using the basics you've already learned, rather than looking up slick search functions that will do the job for you. We'd rather see you write code that you actually understand!
+*/
+
+/* RELEASE 2 - GENERATE RANDOM TEST DATA
+Write a function that takes an integer for length, and builds and returns an array of strings of the given length. 
+So if we ran your function with an argument of 3, we would get an array of 3 random words back (the words don't have to be actual sensical English words -- "nnnnfph" totally counts). 
+The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters. (This involves a new trick, generating a random number, that you'll have to look up, but the solution on how to do so is relatively straightforward.)
+Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
+*/
