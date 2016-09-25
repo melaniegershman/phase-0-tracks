db6ab5ebe097408e06d7c4c23bdff7a60e2f288e
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-25 01:15:09 -0400

    Add user interface; fix percentage print out

--- databases/10000hr/masterit_v2.rb
+++ databases/10000hr/masterit_v2.rb
@@ -55,20 +55,29 @@ end
 def time_remaining(skill_name, db)
   skill_id = skill_name_to_id(db, skill_name)
   hours = db.execute("SELECT hours FROM activity_log WHERE skill_id =(?)", skill_id).flatten
-  hours_sum = hours.reduce(:+)
+  hours_sum = hours.reduce(:+).to_f
   time_remaining = 10000 - hours_sum
-  percent_left = (hours_sum/10000) * 100
-  if percent_left == 100
-    puts "You did it! You're a master of #{skill_name}!"
-  elsif percent_left >= 50 && percent_left < 100
-    puts "Wow, you're over halfway there! You have completed #{percent_left}\% of your skill, and you have #{time_remaining.to_f} hours left until you have mastered #{skill_name}."
-  elsif percent_left >= 25 && percent_left < 50
-    puts "Nice work, you have completed #{percent_left}\% of your skill, and you have #{time_remaining.to_f} hours left until you have mastered #{skill_name}."
-  elsif percent_left >= 1 && percent_left < 25
-    puts "Nice work, you have completed #{percent_left}\% of your skill, and you have #{time_remaining.to_f} hours left until you have mastered #{skill_name}."
-  elsif percent_left < 1
-    puts "You're still a rookie, but keep up the good work! You have #{time_remaining.to_f} hours left until you have mastered #{skill_name}." 
-  end 
+  percent_completed = (hours_sum/10000) * 100
+  # FIGURE OUT HOW TO ROUND PERCENTAGE!!!!!!!
+  flag = 0
+
+  if flag == 0
+    if percent_completed == 100
+      puts "You did it! You're a master of #{skill_name}!"
+      flag = 1
+    elsif percent_completed >= 50
+      puts "Wow, you're over halfway there! You have completed #{percent_completed}\% of your skill, and you have #{time_remaining.to_f} hours left until you have mastered #{skill_name}."
+      flag = 1
+    elsif percent_completed >= 25
+      puts "Nice work, you have completed #{percent_completed}\% of your skill, and you have #{time_remaining.to_f} hours left until you have mastered #{skill_name}."
+      flag = 1
+    elsif percent_completed >= 1
+      puts "Nice work, you have completed #{percent_completed}\% of your skill, and you have #{time_remaining.to_f} hours left until you have mastered #{skill_name}."
+      flag = 1
+    elsif percent_completed < 1
+      puts "You're still a rookie, but keep up the good work! You have #{time_remaining.to_f} hours left until you have mastered #{skill_name}." 
+    end 
+  end
 end
 
 def print_activity_log(log, db)
@@ -80,3 +89,28 @@ def print_skills(db)
   skill_list = db.execute("SELECT * FROM skills")
   skill_list.each {|skill| puts "#{skill[0]}. #{skill[1].capitalize}"}
 end
+
+# =========== User Interface
+puts "Hello! \nMalcolm Gladwell once wrote that it takes 10,000 hours to master a skill. MasterIt is here to help you keep track! \n\nHit ENTER to start."
+user_input = gets.chomp
+until user_input == "no"
+  puts "Enter the skill you have improved:"
+  print_skills(db)
+  skill = []
+  skill_input = gets.chomp
+
+  # check whether input was skill id or skill name
+  skill[0] = skill_id_to_name(db, skill_input.to_i)
+  skill[0] = skill_input if skill[0] == nil
+
+  puts "What did you do to improve #{skill[0]}?"
+  skill[1] = gets.chomp
+  puts "How many HOURS did you spend on '#{skill[1]}'?"
+  skill[2] = gets.chomp
+  activity_log = skill_handler(db, skill)
+  print_activity_log(activity_log, db)
+  time_remaining(skill[0], db)
+  puts "Would you like to enter additional progress? Type yes or no."
+  user_input = gets.chomp.downcase
+end
+puts "Thanks for using MasterIt. Keep working hard!"
