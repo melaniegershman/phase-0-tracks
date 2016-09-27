ab00607a0614fef8e233323958c5149cd3cab8e1
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-26 22:38:10 -0400

    Add event listener for mouseover

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -6,3 +6,26 @@ function addPinkBorder(event) {
 
 var photo = document.getElementById("dog-pic");
 photo.addEventListener("click", addPinkBorder);
+
+
+function mouseOver() {
+    document.getElementById("diamond");
+}
+
+function mouseOut() {
+	document.getElementById("diamond");
+}
+
+function bigImg(x) {
+    gif.style.height = "250px";
+    gif.style.width = "250px";
+}
+
+function normalImg(x) {
+    gif.style.height = "100px";
+    gif.style.width = "100px";
+}
+
+var gif = document.getElementById("diamond");
+gif.addEventListener("mouseover", bigImg);
+gif.addEventListener("mouseout", normalImg);
