0c56d11b5f6f463316027a85428a4f8dc0ece526
Melanie Gershman <melanie.gershman@gmail.com>
2016-09-29 15:48:43 -0400

    Add a /contact route to display an address

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,12 +8,14 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
+=begin
 get '/' do
   "#{params[:name]} is #{params[:age]} years old."
 end
-
+=end
 # write a GET route with
 # route parameters
+=begin
 get '/about/:person' do
   person = params[:person]
   "#{person} is a programmer, and #{person} is learning Sinatra."
@@ -22,9 +24,11 @@ end
 get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
+=end
 
 # write a GET route that retrieves
 # all student data
+=begin
 get '/students' do
   students = db.execute("SELECT * FROM students")
   response = ""
@@ -36,11 +40,23 @@ get '/students' do
   end
   response
 end
+=end
 
 # write a GET route that retrieves
 # a particular student
-
+=begin
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+=end
+
+# ========= Release 0: Add Routes
+# 1. A /contact route that displays an address.
+get '/contact/:person/:contact' do
+    person = params[:person]
+    contact = params[:contact]
+    "#{person} lives at #{contact}."
+end
+# 2. A /great_job route that can take a person's name as a query param.
+
